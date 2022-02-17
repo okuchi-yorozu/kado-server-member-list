d3213b862db9eec776c896a8721d55b0f333fb60
億千よろず <okuchiyorozu@SgMQ8sXi-2.local>
2022-02-18 00:41:22 +0900

    メンバー一覧を SSR 対応にした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
-import {LoginButton, LogoutButton, SaveButton} from "../src/framework/components/Button";
-import {addMember, deleteMember, fetchMembers} from "../src/framework/firebase/store";
-import {useEffect, useState} from "react";
-import {QueryDocumentSnapshot} from "firebase/firestore";
+import {fetchMembers} from "../src/framework/firebase/store";
 import TableCell, {tableCellClasses} from "@mui/material/TableCell";
-import {Box, Paper, Table, TableBody, TableContainer, TableHead, TableRow, TextField, Typography} from "@mui/material";
+import {Paper, Table, TableBody, TableContainer, TableHead, TableRow, TextField, Typography} from "@mui/material";
 import {styled} from "@mui/material/styles";
-import {useAuthContext} from "../src/framework/context/AuthContext";
-import DeleteIcon from "@mui/icons-material/Delete";
+import {DocumentData, QuerySnapshot} from "firebase/firestore";
 
 const StyledTableCell = styled(TableCell)(({theme}) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -27,39 +23,8 @@ const StyledTableRow = styled(TableRow)(({theme}) => ({
     },
 }))
 
-export default function Home() {
-    const user = useAuthContext();
-
-    const [isLoading, setLoading] = useState(true);
-
-    const [members, setMembers] = useState<Array<QueryDocumentSnapshot>>([]);
-    const [no, setNo] = useState('');
-    const [nickname, setNickname] = useState('');
-    const [twitterURL, setTwitterURL] = useState('');
-    const [youTubeURL, setYouTubeURL] = useState('');
-    const [twitchURL, setTwitchURL] = useState('');
-    const [role, setRole] = useState('');
-    const saveFirestore = () => {
-        addMember(parseInt(no,10), nickname, twitterURL, youTubeURL, twitchURL, role).then((docRef) => {
-            console.log({docRef});
-            setLoading(true)
-            setNickname("");
-            setTwitterURL("");
-            setYouTubeURL("");
-            setTwitchURL("");
-            setRole("");
-        })
-    }
-    useEffect(() => {
-        if (isLoading) {
-            fetchMembers().then(snapshot => {
-                    setMembers(snapshot.docs)
-                }
-            )
-        }
-
-        setLoading(false);
-    }, [isLoading])
+// TODO: CSR -> SSR にするとキャッシュが効いて早くなるはず
+export default function Index({members}) {
 
     return (
         <>
@@ -76,32 +41,25 @@ export default function Home() {
                                 <StyledTableCell>名前</StyledTableCell>
                                 <StyledTableCell>リンク</StyledTableCell>
                                 <StyledTableCell>役割</StyledTableCell>
-                                <StyledTableCell></StyledTableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {members ? members.map(doc => {
-                                        const twitterURL = doc.get("twitterURL");
-                                        const youTubeURL = doc.get("youTubeURL");
-                                        const twitchURL = doc.get("twitchURL");
-
-                                        const twitter = twitterURL ? <a href={twitterURL}>
+                            {members ? members.map(member => {
+                                        const twitter = member.twitterURL ? <a href={member.twitterURL}>
                                             <img style={{width: 20, height: 20, marginRight: 8}} src="/images/twitter.png"/>
                                         </a> : "";
-                                        const youTube = youTubeURL ? <a href={youTubeURL}>
+                                        const youTube = member.youTubeURL ? <a href={member.youTubeURL}>
                                             <img style={{width: 20, height: 20, marginRight: 8}} src="/images/youtube.png"/>
                                         </a> : "";
-                                        const twitch = twitchURL ? <a href={twitchURL}>
+                                        const twitch = member.twitchURL ? <a href={member.twitchURL}>
                                             <img style={{width: 20, height: 20, marginRight: 8}} src="/images/twitch.png"/>
                                         </a> : "";
 
-                                        return <StyledTableRow key={doc.id}>
-                                            <StyledTableCell>{doc.get("no")}</StyledTableCell>
-                                            <StyledTableCell>{doc.get("nickname")}</StyledTableCell>
+                                        return <StyledTableRow key={member.id}>
+                                            <StyledTableCell>{member.no}</StyledTableCell>
+                                            <StyledTableCell>{member.nickname}</StyledTableCell>
                                             <StyledTableCell>{twitter}{youTube}{twitch}</StyledTableCell>
-                                            <StyledTableCell>{doc.get("role")}</StyledTableCell>
-                                            <StyledTableCell><DeleteIcon
-                                                onClick={() => deleteMember(doc.id).then(() => setLoading(true))}/></StyledTableCell>
+                                            <StyledTableCell>{member.role}</StyledTableCell>
                                         </StyledTableRow>
                                     }
                                 ) :
@@ -111,73 +69,37 @@ export default function Home() {
                     </Table>
                 </TableContainer>
             </div>
-            <div style={{margin: 8}}>
-                {user.currentUser?.displayName}
-            </div>
-            <div style={{margin: 8}}>
-                {user.currentUser ? <LogoutButton/> : <LoginButton/>}
-            </div>
-            <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-                <div>
-                    <TextField
-                        sx={{m: 1, width: '25ch'}}
-                        label="No."
-                        size="small"
-                        value={no}
-                        onChange={(event) => setNo(event.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <TextField
-                        sx={{m: 1, width: '25ch'}}
-                        label="名前"
-                        size="small"
-                        value={nickname}
-                        onChange={(event) => setNickname(event.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <TextField
-                        sx={{m: 1, width: '25ch'}}
-                        label="Twitter"
-                        size="small"
-                        value={twitterURL}
-                        onChange={(event) => setTwitterURL(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        sx={{m: 1, width: '25ch'}}
-                        label="YouTube"
-                        size="small"
-                        value={youTubeURL}
-                        onChange={(event) => setYouTubeURL(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        sx={{m: 1, width: '25ch'}}
-                        label="Twitch"
-                        size="small"
-                        value={twitchURL}
-                        onChange={(event) => setTwitchURL(event.target.value)}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        sx={{m: 1, width: '25ch'}}
-                        label="役割"
-                        size="small"
-                        value={role}
-                        onChange={(event) => setRole(event.target.value)}
-                    />
-                </div>
-            </Box>
-            <div style={{margin: 8}}>
-                <SaveButton onclick={saveFirestore}/>
-            </div>
         </>
     )
 }
+
+type Member = {
+    id: string,
+    no: number,
+    nickname: string,
+    twitterURL: string,
+    youTubeURL: string,
+    twitchURL: string,
+    role: string,
+}
+
+// Index ページがリクエストされた場合に、メンバー一覧を取得して、クライアントにページを返す
+export async function getServerSideProps() {
+    const toJSON = (querySnapshot: QuerySnapshot<DocumentData>): Member[] => {
+        return querySnapshot.docs.map(row => ({
+            id: row.id,
+            no: row.get("no"),
+            nickname: row.get("nickname"),
+            twitterURL: row.get("twitterURL"),
+            youTubeURL: row.get("youTubeURL"),
+            twitchURL: row.get("twitchURL"),
+            role: row.get("role"),
+        }))
+    }
+
+    const querySnapshot = await fetchMembers();
+    const members = toJSON(querySnapshot);
+
+    return {props: {members}};
+}
+
